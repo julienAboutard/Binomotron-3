896876e001bb0d7c2ffdb7a314a5af0a04af98a8
Julien Aboutard <julien.aboutard@gmail.com>
2022-11-10 16:09:29 +0100

    suite config

--- sitebinomotron/sitebinomotron/settings.py
+++ sitebinomotron/sitebinomotron/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'binomotron.apps.BinomotronConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
