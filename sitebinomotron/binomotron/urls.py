48b38ce4c3aa04e11b1869dfbad33bb6faf88084
Julien Aboutard <julien.aboutard@gmail.com>
2022-11-10 16:31:43 +0100

    Fin de la config de l'arborescense projet

--- sitebinomotron/binomotron/urls.py
+++ sitebinomotron/binomotron/urls.py
@@ -6,9 +6,9 @@ app_name = 'binomotron'
 
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
 
     path('', views.ApprenantView.as_view(), name='apprenant'),
 
