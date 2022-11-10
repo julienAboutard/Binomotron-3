c62ec6219d0141a432f786ebe15ed2bcc335dc78
Julien Aboutard <julien.aboutard@gmail.com>
2022-11-10 16:06:26 +0100

    suite de config django

--- sitebinomotron/sitebinomotron/settings.py
+++ sitebinomotron/sitebinomotron/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'sitebinomotron.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'binomotron',
+        'HOST':"localhost",
+        'USER':"audrey",
+        'PASSWORD':"root",
+        'TEST': {
+            'NAME': 'test_binomotron',
+        },
     }
 }
 
