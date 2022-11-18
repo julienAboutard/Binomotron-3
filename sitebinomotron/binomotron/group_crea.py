91c23bf70d2228bfb6e482d63b84f8a9fc1d008a
Julien Aboutard <julien.aboutard@gmail.com>
2022-11-18 15:13:40 +0100

    Commentaire du code

--- sitebinomotron/binomotron/group_crea.py
+++ sitebinomotron/binomotron/group_crea.py
@@ -6,16 +6,20 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 
 
 def groupe_create(brief_id):
-    # Récupération de l'ensembles des apprenants
+    # Récupération de l'ensembles des apprenants et du brief concerné et s'ils n'existent pas on retourne un erreur 404
     students = get_list_or_404(Apprenant)
     brief_selec = get_object_or_404(Brief, pk = brief_id)
     
     res = []
     liste_groupes = []
 
+    # Si la taille des groupes pour le brief est supérieur aux nombres d'apprenants on retourne un unique groupe de 
+    # de tous les apprenants
     if brief_selec.nombre >= len(students):
         liste_groupes.append(students)
-    elif brief_selec.nombre >= len(students)/2 and brief_selec.nombre < len(students):
+    # Si l a taille des groupes pour le brief est plus grand que la moitié du nombre d'apprenants, on retourne 2 groupes 
+    # taille plus ou moins équivalente (on exclus les cas pour un nombre d'apprenant <= 3)
+    elif brief_selec.nombre >= len(students)/2 and brief_selec.nombre < len(students) and  len(students) > 3:
         for i in range(len(students)//2):
             student = random.choice(students)
             res.append(student)
@@ -23,13 +27,15 @@ def groupe_create(brief_id):
         liste_groupes.append(res)
         liste_groupes.append([student for student in students])
     else :
+        # S'il existe un reste pour la division euclidienne du nombre d'apprenant par la taille des n-omes, on extrait 
+        # au hasard un nombre d'apprenant équivalent au reste
         if len(students) % brief_selec.nombre != 0:
             reste = len(students) % brief_selec.nombre
             while reste > 0 :
                 random.shuffle(students)
                 res.append(students.pop())
                 reste -= 1
-        
+        # On crée les groupes sur la liste des apprenants restant au hasard
         while len(students) > 0 :
             count = 0
             group = []
@@ -38,10 +44,13 @@ def groupe_create(brief_id):
                 group.append(students.pop())
                 count += 1
             liste_groupes.append(group)
-        
+        # Si la liste res est pas vide 
         if len(res) != 0 :
+            # On vérifie que la taille de cette liste soit inférieur de 1 à la taille des n-omes et 
+            #  si oui, on injecte ce reste d'apprenants en tant que groupe
             if len(res) == (len(liste_groupes[0]) - 1) and (len(liste_groupes[0]) >= 3):
                 liste_groupes.append(res)
+            # Si non, on insère les apprenants restants dans les autres groupes au hasard
             else:
                 while len(res) > 0:
                     id = []
